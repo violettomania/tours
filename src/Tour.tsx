1c763c5ed6741cf741a46d25e73cbe3107a34d7d
violettomania <danielpolya@protonmail.com>
2023-10-30 18:45:24 +0100

    some css changes

--- src/Tour.tsx
+++ src/Tour.tsx
@@ -4,12 +4,12 @@ function Tour({ image, info, name, price }: SingleTour) {
   return (
     <article className='single-tour'>
       <img src={image} alt={name} className='img'></img>
-      <span className='tour-price'>{price}</span>
+      <span className='tour-price'>{`$${price}`}</span>
       <div className='tour-info'>
         <h3>{name}</h3>
         <p>
           {info}
-          <button className='info-btn'>read more</button>
+          <button className='info-btn'>...read more</button>
         </p>
         <button className='delete-btn btn-block btn'>not interested</button>
       </div>
