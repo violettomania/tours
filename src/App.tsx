0be73b50db26bd812298cc953183aa327f76c8bc
violettomania <danielpolya@protonmail.com>
2023-11-04 11:07:49 +0100

    refactor: update no tours available text

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
         </div>
         <div className='tours'>
           {tours?.length === 0 ? (
-            <p>no tours available</p>
+            <p>No tours available.</p>
           ) : (
             tours?.map((tour) => (
               <Tour
